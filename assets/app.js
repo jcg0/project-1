495a1969c4c1b8d5c1f9472e33899202047e4695
jcg0 <jg.awsprod@gmail.com>
2022-11-17 10:36:54 -0500

    refactored app js code to use axios

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,36 @@
+const appId = "&app_id=287a773f";
+const appKey = "&app_key=080726096a3b8d16c8a969402882bab9";
+
+async function getRecipeApi() {
+  try {
+    const response = await axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=alfredo${appId}${appKey}`
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
+
 // const options = {
 
 // }
 
-function getRecipeApi() {
-  const requestUrl =
-    "https://api.edamam.com/api/recipes/v2?type=public&q=alfredo&app_id=287a773f&app_key=080726096a3b8d16c8a969402882bab9";
+// function getRecipeApi() {
+//   const requestUrl =
+//     "https://api.edamam.com/api/recipes/v2?type=public&q=alfredo&app_id=287a773f&app_key=080726096a3b8d16c8a969402882bab9";
 
-  fetch(requestUrl).then((res) => {
-    console.log("Success", res);
-    res.json().then((data) => console.log("JSON", data));
-  });
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-}
+//   fetch(requestUrl)
+//     .then((res) => {
+//       console.log("Success", res);
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log("JSON", data);
+//     });
+//     .then((data) => {
+//       console.log(data);
+//     });
+//}
 
 getRecipeApi();
