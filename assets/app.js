4825a313160fda3bd6f12d44f6c26c4c6df46b0f
jcg0 <jg.awsprod@gmail.com>
2022-11-29 14:11:49 -0500

    created css selector for box shadow

--- assets/app.js
+++ assets/app.js
@@ -9,15 +9,13 @@ const mealImg3 = document.querySelector("#mealImg3");
 const mealImg4 = document.querySelector("#mealImg4");
 const mealImg5 = document.querySelector("#mealImg5");
 
-const cocktailCard = $('#cocktailData');
-const cocktailIngrd = $('#cocktailIngredients')
+const cocktailCard = $("#cocktailData");
+const cocktailIngrd = $("#cocktailIngredients");
 
-
-const randRecipe = document.querySelector('#random-list')
-const randButton = document.querySelector('#get-random-recipe')
-const menuContentEl = document.querySelector('#mealData')
-const randFoodDataUl = document.querySelector('#randomFoodData');
-
+const randRecipe = document.querySelector("#random-list");
+const randButton = document.querySelector("#get-random-recipe");
+const menuContentEl = document.querySelector("#mealData");
+const randFoodDataUl = document.querySelector("#randomFoodData");
 
 mealForm.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -44,88 +42,9 @@ mealForm.addEventListener("submit", function (event) {
 
       showMealRecipe(data);
 
-      showRandomRecipe(data);
-
-
       return;
     });
 });
-
-//random recipe fetch
-randButton.addEventListener('click', function(){
-  const randParam = randRecipe.selectedOptions[0].value
-    
-    fetch(
-      `https://api.edamam.com/api/recipes/v2?type=public${appId}${appKey}&cuisineType=${randParam}&random=true`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        randFoodDataUl.innerHTML=''
-        showRandomRecipe(data);
-        populateRandRecipe(data);
-        console.log("foodRandom", data);
-        return
-      })
-})
-
-const showRandomRecipe = function (data) {
-  const largeImg1 = data.hits[0].recipe.image;
-  const largeImg2 = data.hits[1].recipe.image;
-  const largeImg3 = data.hits[2].recipe.image;
-  const largeImg4 = data.hits[3].recipe.image;
-  const largeImg5 = data.hits[4].recipe.image;
-  
-  mealImg1.setAttribute("src", largeImg1);
-  mealImg2.setAttribute("src", largeImg2);
-  mealImg3.setAttribute("src", largeImg3);
-  mealImg4.setAttribute("src", largeImg4);
-  mealImg5.setAttribute("src", largeImg5);
-}
-
-const populateRandRecipe = function (data) {
-  for (let i = 0; i < data.hits.length; i++) {
-    const randDiv = document.createElement('div');
-    const randP = document.createElement("p");
-    const randImg = document.createElement("img");
-    const randAnchor = document.createElement("a");
-
-    const recipeUrl = data.hits[i].recipe.url;
-    const recipeLabel = data.hits[i].recipe.label;
-    const thumbImg = data.hits[i].recipe.images.THUMBNAIL.url;
-
-    const drpdwnDivParent = document.createElement("div");
-    const drpdwnDivChild = document.createElement("div");
-    const drpdwnBtn = document.createElement("button");
-
-    drpdwnDivParent.classList.add("uk-inline");
-    drpdwnBtn.classList.add("uk-button", "uk-button-default");
-    drpdwnBtn.setAttribute("type", "button");
-    drpdwnDivChild.setAttribute("uk-dropdown", "mode: hover");
-
-    randImg.classList.add("img");
-    randAnchor.classList.add("uk-align-center", "meal-anchor", "meal-anchor:hover");
-    randP.classList.add("label-p");
-
-    randDiv.classList.add( "meal-list", "uk-card", "uk-card-default", "uk-card-body");
-
-    randAnchor.href = recipeUrl;
-    randImg.src = thumbImg;
-
-    randAnchor.textContent = "Get Recipe!";
-    randP.textContent = recipeLabel;
-
-    randAnchor.setAttribute("target", "_blank");
-
-    randDiv.append(randP);
-    randDiv.append(randImg);
-    randDiv.append(randAnchor);
-
-    randFoodDataUl.append(randDiv);
-  }
-
-}
 
 const showRecipeCarousel = function (data) {
   const largeImg1 = data.hits[0].recipe.image;
@@ -169,18 +88,19 @@ const showMealRecipe = function (data) {
     const protein = Math.round(data.hits[i].recipe.digest[2].total);
 
     drpdwnDivParent.classList.add("uk-inline");
-    drpdwnBtn.classList.add("uk-button", "uk-button-default");
+    drpdwnBtn.classList.add("uk-button", "uk-button-default", "box-shadow");
     drpdwnUl.classList.add("new-ul");
     img.classList.add("img");
     aTag.classList.add("uk-align-center", "meal-anchor", "meal-anchor:hover");
     labelP.classList.add("label-p");
     //Use these to populate results onto page.
-    div.classList.add( 
+    div.classList.add(
       "meal-list",
       "uk-card",
       "uk-card-default",
-      "uk-card-body"
-    ); 
+      "uk-card-body",
+      "box-shadow"
+    );
 
     aTag.href = recipeUrl;
     img.src = thumbImg;
@@ -225,12 +145,93 @@ const showMealRecipe = function (data) {
   }
 };
 
+//random recipe fetch
+randButton.addEventListener("click", function () {
+  const randParam = randRecipe.selectedOptions[0].value;
 
+  fetch(
+    `https://api.edamam.com/api/recipes/v2?type=public${appId}${appKey}&cuisineType=${randParam}&random=true`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      randFoodDataUl.innerHTML = "";
+      showRandomRecipe(data);
+      populateRandRecipe(data);
+      console.log("foodRandom", data);
+      return;
+    });
+});
+
+const showRandomRecipe = function (data) {
+  const largeImg1 = data.hits[0].recipe.image;
+  const largeImg2 = data.hits[1].recipe.image;
+  const largeImg3 = data.hits[2].recipe.image;
+  const largeImg4 = data.hits[3].recipe.image;
+  const largeImg5 = data.hits[4].recipe.image;
+
+  mealImg1.setAttribute("src", largeImg1);
+  mealImg2.setAttribute("src", largeImg2);
+  mealImg3.setAttribute("src", largeImg3);
+  mealImg4.setAttribute("src", largeImg4);
+  mealImg5.setAttribute("src", largeImg5);
+};
+
+const populateRandRecipe = function (data) {
+  for (let i = 0; i < data.hits.length; i++) {
+    const randDiv = document.createElement("div");
+    const randP = document.createElement("p");
+    const randImg = document.createElement("img");
+    const randAnchor = document.createElement("a");
+
+    const recipeUrl = data.hits[i].recipe.url;
+    const recipeLabel = data.hits[i].recipe.label;
+    const thumbImg = data.hits[i].recipe.images.THUMBNAIL.url;
+
+    const drpdwnDivParent = document.createElement("div");
+    const drpdwnDivChild = document.createElement("div");
+    const drpdwnBtn = document.createElement("button");
+
+    drpdwnDivParent.classList.add("uk-inline");
+    drpdwnBtn.classList.add("uk-button", "uk-button-default");
+    drpdwnBtn.setAttribute("type", "button");
+    drpdwnDivChild.setAttribute("uk-dropdown", "mode: hover");
+
+    randImg.classList.add("img");
+    randAnchor.classList.add(
+      "uk-align-center",
+      "meal-anchor",
+      "meal-anchor:hover"
+    );
+    randP.classList.add("label-p");
+
+    randDiv.classList.add(
+      "meal-list",
+      "uk-card",
+      "uk-card-default",
+      "uk-card-body"
+    );
+
+    randAnchor.href = recipeUrl;
+    randImg.src = thumbImg;
+
+    randAnchor.textContent = "Get Recipe!";
+    randP.textContent = recipeLabel;
+
+    randAnchor.setAttribute("target", "_blank");
+
+    randDiv.append(randP);
+    randDiv.append(randImg);
+    randDiv.append(randAnchor);
+
+    randFoodDataUl.append(randDiv);
+  }
+};
 
 // function getCocktail(data){
 //   for (let i = 0; i < data.drinks.length; i++) {
-    
-    
+
 //     const div = $('<div>');
 //     const paragraph = $('<p>');
 //     const pDrinkIngredient = $('<p>')
@@ -238,22 +239,22 @@ const showMealRecipe = function (data) {
 //     div.addClass('meal-list uk-card uk-card-default uk-card-body')
 //     div.text(data.drinks[i].strDrink);
 //     pDrinkInst.text(data.drinks[i].strInstructions);
-     
+
 //     cocktailCard.append(div);
 //     div.append(paragraph);
 //     div.append(pDrinkIngredient);
 //     div.append(pDrinkInst);
-    
+
 //   }
 // }
 
 // cocktail search function.
-$(document).ready(function(){
+$(document).ready(function () {
   $("#cocktailSearchForm").on("submit", function (event) {
     event.preventDefault();
     var cocktailSearch = $("#inputValue").val().trim();
     console.log(cocktailSearch);
-  
+
     const options = {
       method: "GET",
       headers: {
@@ -261,7 +262,7 @@ $(document).ready(function(){
         "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
       },
     };
-  
+
     fetch(
       "https://the-cocktail-db.p.rapidapi.com/search.php?s=" + cocktailSearch,
       options
@@ -272,44 +273,39 @@ $(document).ready(function(){
       .then(function (data) {
         console.log(data);
         const drinks = Object.assign({}, data);
-        console.log(drinks.drinks)
+        console.log(drinks.drinks);
 
-        
-        
         for (let i = 0; i < drinks.drinks.length; i++) {
           const ingredient = drinks.drinks[i];
-            console.log('drinkName'+ingredient['strDrink'])
-            const div = $('<div>');
-            const paragraph = $('<p>');
-            const pDrinkIngredient = $('<p>')
-            const pDrinkInst = $('<p>')
-            div.addClass('meal-list uk-card uk-card-default uk-card-body')
-            div.text(data.drinks[i].strDrink);
-            pDrinkInst.text(data.drinks[i].strInstructions);
-            
-            cocktailCard.append(div);
-            div.append(paragraph);
-            div.append(pDrinkIngredient);
-            div.append(pDrinkInst);
-          for (let j = 1; j < drinks.drinks.length -1; j++) {
-            console.log('drinkName'+ingredient['strDrink'])
-            const div = $('<div>');
-            div.addClass('meal-list uk-card uk-card-default uk-card-body')
-            
+          console.log("drinkName" + ingredient["strDrink"]);
+          const div = $("<div>");
+          const paragraph = $("<p>");
+          const pDrinkIngredient = $("<p>");
+          const pDrinkInst = $("<p>");
+          div.addClass("meal-list uk-card uk-card-default uk-card-body");
+          div.text(data.drinks[i].strDrink);
+          pDrinkInst.text(data.drinks[i].strInstructions);
 
-            const pDrinkIngredient = $('<p>')
+          cocktailCard.append(div);
+          div.append(paragraph);
+          div.append(pDrinkIngredient);
+          div.append(pDrinkInst);
+          for (let j = 1; j < drinks.drinks.length - 1; j++) {
+            console.log("drinkName" + ingredient["strDrink"]);
+            const div = $("<div>");
+            div.addClass("meal-list uk-card uk-card-default uk-card-body");
+
+            const pDrinkIngredient = $("<p>");
             // cocktailCard.append(div);
             div.append(pDrinkIngredient);
-            pDrinkIngredient.text(ingredient['strIngredient'+j])
-            cocktailCard.append(pDrinkIngredient)
-            
-            if (ingredient['strIngredient'+j] != null)
-            
-            console.log(ingredient['strIngredient'+j])
+            pDrinkIngredient.text(ingredient["strIngredient" + j]);
+            cocktailCard.append(pDrinkIngredient);
+
+            if (ingredient["strIngredient" + j] != null)
+              console.log(ingredient["strIngredient" + j]);
           }
-        
         }
-  
+
         const drinkImg = [
           data.drinks[0].strDrinkThumb,
           data.drinks[1].strDrinkThumb,
@@ -317,66 +313,64 @@ $(document).ready(function(){
           data.drinks[3].strDrinkThumb,
           data.drinks[4].strDrinkThumb,
         ];
-      //   //This loop will run through the ingredients needed for the drink.
-      //   for (let i = 1; i < drinkList.length; i++) {
-      //     // console.log(i);
-  
-      //     ingredient = data.drinks[0][`strIngredient${i}`];
-  
-      //     console.log(ingredient);
-      //     $('#drinkIngredient').append(' ' + ingredient + '');
-      //   }
-  
-      //   for (let i = 1; i < drinkList.length; i++) {
-      //     //console.log(i);
-      //     thumbNail = data.drinks[i].strDrinkThumb;
-      //     $('#cocktailImage').attr('src', thumbNail);
-      //     console.log(thumbNail)
-      //     // console.log(ingredient1);
-      //   }
-  
-      //   // for (let i = 1; i < drinkList.length; i++) {
-      //   //   //console.log(i);
-      //   //   ingredient2 = data.drinks[2][`strIngredient${i}`];
-      //   //   console.log(ingredient2);
-      //   // }
-  
-      //   // for (let i = 1; i < drinkList.length; i++) {
-      //   //   //console.log(i);
-      //   //   ingredient3 = data.drinks[3][`strIngredient${i}`];
-      //   //   console.log(ingredient3);
-      //   // }
-  
-      //   // for (let i = 1; i < drinkList.length; i++) {
-      //   //   //console.log(i);
-      //   //   ingredient4 = data.drinks[4][`strIngredient${i}`];
-      //   //   console.log(ingredient4);
-      //   // }
+        //   //This loop will run through the ingredients needed for the drink.
+        //   for (let i = 1; i < drinkList.length; i++) {
+        //     // console.log(i);
 
-      $('#cocktailImage').attr('src', drinkImg[0]);
-      $('#cocktailImage1').attr('src', drinkImg[1]);
-      $('#cocktailImage2').attr('src', drinkImg[2]);
-      $('#cocktailImage3').attr('src', drinkImg[3]);
-      $('#cocktailImage4').attr('src', drinkImg[4]);
+        //     ingredient = data.drinks[0][`strIngredient${i}`];
 
-  
-  
-      //   // this loop is only bring back null, not sure why yet.
-      //   // for (let i = 1; i < 16; i++) {
-      //   //   //console.log(i);
-      //   //   measure = data.drinks[0][`strMeasure${i}`];
-      //   //   console.log(measure);
-      //   // };
-  
-      //   // const imgDrink = document.createElement("img");
-      //   // imgDrink.src = data.drinks[1].strDrinkThumb;
-      //   // let img_Drink = document.getElementById("cocktailImg");
-      //   // img_Drink.append(imgDrink);
-  
-      //   // cocktailSearchForm.append(element)
+        //     console.log(ingredient);
+        //     $('#drinkIngredient').append(' ' + ingredient + '');
+        //   }
+
+        //   for (let i = 1; i < drinkList.length; i++) {
+        //     //console.log(i);
+        //     thumbNail = data.drinks[i].strDrinkThumb;
+        //     $('#cocktailImage').attr('src', thumbNail);
+        //     console.log(thumbNail)
+        //     // console.log(ingredient1);
+        //   }
+
+        //   // for (let i = 1; i < drinkList.length; i++) {
+        //   //   //console.log(i);
+        //   //   ingredient2 = data.drinks[2][`strIngredient${i}`];
+        //   //   console.log(ingredient2);
+        //   // }
+
+        //   // for (let i = 1; i < drinkList.length; i++) {
+        //   //   //console.log(i);
+        //   //   ingredient3 = data.drinks[3][`strIngredient${i}`];
+        //   //   console.log(ingredient3);
+        //   // }
+
+        //   // for (let i = 1; i < drinkList.length; i++) {
+        //   //   //console.log(i);
+        //   //   ingredient4 = data.drinks[4][`strIngredient${i}`];
+        //   //   console.log(ingredient4);
+        //   // }
+
+        $("#cocktailImage").attr("src", drinkImg[0]);
+        $("#cocktailImage1").attr("src", drinkImg[1]);
+        $("#cocktailImage2").attr("src", drinkImg[2]);
+        $("#cocktailImage3").attr("src", drinkImg[3]);
+        $("#cocktailImage4").attr("src", drinkImg[4]);
+
+        //   // this loop is only bring back null, not sure why yet.
+        //   // for (let i = 1; i < 16; i++) {
+        //   //   //console.log(i);
+        //   //   measure = data.drinks[0][`strMeasure${i}`];
+        //   //   console.log(measure);
+        //   // };
+
+        //   // const imgDrink = document.createElement("img");
+        //   // imgDrink.src = data.drinks[1].strDrinkThumb;
+        //   // let img_Drink = document.getElementById("cocktailImg");
+        //   // img_Drink.append(imgDrink);
+
+        //   // cocktailSearchForm.append(element)
       });
   });
-})
+});
 
 // cocktail search function.
 
@@ -533,22 +527,18 @@ $(document).ready(function () {
   });
 });
 
+randButton.addEventListener("click", function () {
+  const randParam = randRecipe.selectedOptions[0].value;
 
-
-randButton.addEventListener('click', function(){
-  const randParam = randRecipe.selectedOptions[0].value
-    
-    fetch(
-      `https://api.edamam.com/api/recipes/v2?type=public${appId}${appKey}&cuisineType=${randParam}&random=true`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        showRandomRecipe(data);
-        console.log("foodRandom", data);
-        return
-      })
-})
-
-
+  fetch(
+    `https://api.edamam.com/api/recipes/v2?type=public${appId}${appKey}&cuisineType=${randParam}&random=true`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      showRandomRecipe(data);
+      console.log("foodRandom", data);
+      return;
+    });
+});
